9ae3bd74f578989dce5d0871c9e1ce3347c326d9
renjingj <renjingj@gmail.com>
2016-11-11 15:38:24 -0800

    Change ace to src-min-noconflict version

--- static/js/snippets/haskell_cabal.js
+++ static/js/snippets/haskell_cabal.js
@@ -1,7 +1,1 @@
-define("ace/snippets/haskell_cabal",["require","exports","module"], function(require, exports, module) {
-"use strict";
-
-exports.snippetText =undefined;
-exports.scope = "haskell_cabal";
-
-});
+ace.define("ace/snippets/haskell_cabal",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="haskell_cabal"})
