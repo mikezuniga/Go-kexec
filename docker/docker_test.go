c7d795bf906a84f1f06be31d778a6e335fcc49cb
renjingj <renjingj@gmail.com>
2016-11-23 22:03:35 -0800

    Add a link; modified a test

--- docker/docker_test.go
+++ docker/docker_test.go
@@ -6,10 +6,14 @@ import (
 
 func TestBuildFunction(t *testing.T) {
 	d, _ := NewClient("unix:///var/run/docker.sock")
-	d.BuildFunction("xuant", "faas:v1", "python27")
+	if err := d.BuildFunction("registry.paas.symcpe.com:443", "jingjing_ren", "faas:v1", "python27", "example/"); err != nil {
+		t.Error(err)
+	}
 }
 
 func TestRegisterFunction(t *testing.T) {
 	d, _ := NewClient("unix:///var/run/docker.sock")
-	d.RegisterFunction("xuant", "faas:v1")
+	if err := d.RegisterFunction("registry.paas.symcpe.com:443", "jingjing_ren", "faas:v1"); err != nil {
+		t.Error(err)
+	}
 }
